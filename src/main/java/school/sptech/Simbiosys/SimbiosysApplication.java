5527aab09af6fe3bc4978111a93d737da3539975
ViSuliman <vitoria.suliman@sptech.school>
2025-03-09 17:56:49 -0300

    add essential Spring dependencies

---
+++ src/main/java/school/sptech/Simbiosys/SimbiosysApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.Simbiosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbiosysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimbiosysApplication.class, args);
+	}
+
+}
