2802759c81ccf7dab58367d0e25a7e1c219ac460
cintiaohara <cintia.ohara@sptech.school>
2025-04-06 22:45:28 -0300

    adicionando DTOs e criando a service do Usuario

---
+++ src/main/java/school/sptech/Simbiosys/exception/EntidadeJaExistente.java
@@ -0,0 +1,8 @@
+package school.sptech.Simbiosys.exception;
+
+public class EntidadeJaExistente extends RuntimeException {
+
+    public EntidadeJaExistente(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/school/sptech/Simbiosys/service/AlterarSenhaDto.java
@@ -0,0 +1,13 @@
+package school.sptech.Simbiosys.service;
+
+public class AlterarSenhaDto {
+    private String novaSenha;
+
+    public String getNovaSenha() {
+        return novaSenha;
+    }
+
+    public void setNovaSenha(String novaSenha) {
+        this.novaSenha = novaSenha;
+    }
+}
