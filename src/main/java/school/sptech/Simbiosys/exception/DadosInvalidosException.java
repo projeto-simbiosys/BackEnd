8442bb0ececcc5280f836a69a02f3b143a87a1af
Reynald-Costa <reynald.costa@sptech.school>
2025-04-18 20:51:13 -0300

    Ajustes nos endponits do RelatorioController

---
+++ src/main/java/school/sptech/Simbiosys/exception/DadosInvalidosException.java
@@ -0,0 +1,8 @@
+package school.sptech.Simbiosys.exception;
+
+public class DadosInvalidosException extends RuntimeException {
+
+    public DadosInvalidosException(String message) {
+        super(message);
+    }
+}
