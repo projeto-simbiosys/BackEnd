d0e19f3d279edbebc6430ebb9c2e2e1911c553a0
Reynald-Costa <reynald.costa@sptech.school>
2025-04-18 15:26:24 -0300

    Ajustes do JWT no Swagger

--- src/main/java/school/sptech/Simbiosys/config/SwaggerConfig.java
+++ src/main/java/school/sptech/Simbiosys/config/SwaggerConfig.java
@@ -1,20 +1,27 @@
 package school.sptech.Simbiosys.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @OpenAPIDefinition(
         info = @Info(
                 title = "Simbiosys API",
                 version = "1.0",
                 description = "Documentação API REST"
-        )
+        ),
+        security = @SecurityRequirement(name = "Bearer")
 )
 
-@Configuration
+@SecurityScheme(
+        name = "Bearer", type   = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT"
+)
 public class SwaggerConfig {
 
     @Bean
