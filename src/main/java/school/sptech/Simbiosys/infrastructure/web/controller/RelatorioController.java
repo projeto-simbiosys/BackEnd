d1a985acf15aa651c07d4b8794135afb4efbb7c4
cintiaohara <cintia.ohara@sptech.school>
2025-11-04 18:39:10 -0300

    adicionando endpoint

--- src/main/java/school/sptech/Simbiosys/infrastructure/web/controller/RelatorioController.java
+++ src/main/java/school/sptech/Simbiosys/infrastructure/web/controller/RelatorioController.java
@@ -110,6 +110,30 @@ public class RelatorioController {
         return ResponseEntity.ok(relatorios);
     }
 
+    @GetMapping("/listar")
+    public ResponseEntity<Page<RelatorioEntity>> listarTodos(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "24") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String direction
+    ) {
+        Pageable pageable = PageRequest.of(
+                page,
+                size,
+                direction.equalsIgnoreCase("asc")
+                        ? Sort.by(sortBy).ascending()
+                        : Sort.by(sortBy).descending()
+        );
+
+        Page<RelatorioEntity> relatorios = buscarRelatoriosUseCase.execute(pageable);
+
+        if (relatorios.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(relatorios);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<RelatorioEntity> buscarPorId(@PathVariable Integer id) {
         try {
